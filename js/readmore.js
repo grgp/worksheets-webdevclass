2803396cac240f742feccb20c306303fff202b27
grgp <g.a.pitoy@gmail.com>
2015-10-08 19:38:49 +0700

    external js

--- js/readmore.js
+++ js/readmore.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
-	$("#readmoreb").click(function(){
-		$('#addme').load("readmoreHome.txt");
-		$('#addme').load("js/ajax/readmoreHome.txt");
-	})
-})
+  $("#readmore_07").click(function(){
+      $("#post_07").load("misc/ajax/content_readMore_07.txt").hide().fadeIn(1000);
+      $("#readmore_07").hide();
+  });
+
+  $("#readmore_08").click(function(){
+      $("#post_08").load("misc/ajax/content_readMore_08.txt").hide().fadeIn(1000);
+      $("#readmore_08").hide();
+  });
+
+  $("#readmore_06").click(function(){
+      $("#post_06").load("misc/ajax/content_readMore_06.txt").hide().fadeIn(1000);
+      $("#readmore_06").hide();
+  });
+
+});

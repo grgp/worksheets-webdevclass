6d30b375f9254dce334638d741ae59b45f4b0473
grgp <g.a.pitoy@gmail.com>
2015-10-27 20:52:46 +0700

    finished more posts

--- js/showposts.js
+++ js/showposts.js
@@ -1,15 +1,43 @@
-function showposts() {
-	console.log("erere");
+function cleansession() {
+	sessionStorage.setItem("counter", 0);
+}
+
+function showposts(n) {
+	var counter = 0;
+	if (sessionStorage.getItem("counter")) {
+		counter = Number(sessionStorage.getItem("counter"));
+	}
+	else {
+		sessionStorage.setItem("counter", 0);
+	}
+
 	$.getJSON("misc/json/posts.json", function(json) {
-		$.each(json, function (i, post) {
+		var outerCounter = counter;
+		for (var j = counter; j < n+outerCounter && j < json.length; j++) {
+			var post = json[j];
+			console.log("counter: " + outerCounter + " | n: " + n + " | j: " + j + " | counter: " + counter);
 			var postcell = (
-					'<div class="outercard">' +
+					'<div class="outercard grow3">' +
 						'<article id="post_' + post.id + '">' +
 							'<header> <h2><a href="' + post.link + '">' + post.title + '</a></h2>' +
 							'<p><i> by ' + post.author + '  ---  posted ' + post.date + '</i></p>' + '</header>' + post.content +
 						'</article></div>'
 				);
-			$('#postlist').prepend(postcell);
-		});
+			$('#postlist').append(postcell);
+			counter = counter + 1;
+			if (j == json.length - 1) {
+				console.log("reachme");
+				deleteLoadButton();
+			}
+		}
+		sessionStorage.setItem("counter", counter);
 	});
+}
+
+function deleteLoadButton() {
+	document.getElementById('loadButton').innerHTML = "";
+}
+
+function showadditional() {
+	showposts(4);
 }
